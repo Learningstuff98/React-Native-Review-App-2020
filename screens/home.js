9f82a58630fbd9b257799f291369d52c4846874c
Andy Strube <andystrube64@gmail.com>
2020-08-19 23:03:10 -0700

    Added some custom fonts

--- screens/home.js
+++ screens/home.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function Home() {
-  return(
-    <View>
-      <Text style={styles.container}>
-        Home Screen
-      </Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Home Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24    
+    padding: 24,
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
   }
 });
