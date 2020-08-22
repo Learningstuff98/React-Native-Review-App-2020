ea2fa0b7fc0183146f35b8f41f334217670459a0
Andy Strube <andystrube64@gmail.com>
2020-08-21 19:05:44 -0700

    Navigation works sort of

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { styles } from '../styles/styles';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+  const pressHandler = () => {
+    navigation.navigate('reviewDetails');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Home Screen</Text>
+      <Button
+        title='go to review details'
+        onPress={pressHandler}
+      />
     </View>
   );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { styles } from '../styles/styles';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+  const pressHandler = () => {
+    //navigation.goBack();
+    navigation.navigate('home');
+  }
+
   return(
     <View>
       <Text style={styles.container}>
         ReviewDetails Screen
       </Text>
+      <Button
+        title='go to home'
+        onPress={pressHandler}
+      />
     </View>
   );
 }
